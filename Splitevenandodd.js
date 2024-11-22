411f014934ee798f41a8f57c11686a8175c84577
Ghulam Ahmed <dev.ahmed09@gmail.com>
2024-11-22 16:53:30 +0500

    Create Splitevenandodd.js

---
+++ Splitevenandodd.js
@@ -0,0 +1,9 @@
+let num = [12,18,19,21,15,17,31]
+for(let i=0;i<num.length;i++){
+    if(num[i] % 2 ===0){
+        console.log(num[i],"is even");
+    }
+    else{
+        console.log(num[i],"is odd");
+    }
+}
