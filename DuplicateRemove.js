475d4e18deda764462ae8065bee99fc3a3204dfe
Ghulam Ahmed <dev.ahmed09@gmail.com>
2024-11-26 13:39:56 +0500

    .

--- DuplicateRemove.js
+++ DuplicateRemove.js
@@ -1,10 +1,10 @@
 let nums = [12, 34, 56, 34, 77, 90, 27, 56, 45, 67, 89, 10, 34, 12];
-let uniqueNums = []; // Empty array to store unique numbers
+let uniqueNums = []; 
 
 for (let i = 0; i < nums.length; i++) {
   if (!uniqueNums.includes(nums[i])) {
-    // Check if the number is not already in uniqueNums
-    uniqueNums.push(nums[i]); // Add it to uniqueNums if it's not a duplicate
+    
+    uniqueNums.push(nums[i]); 
   }
 }
 
---
+++ ReverseName.js
@@ -0,0 +1,8 @@
+// Input name
+let name = "Ghulam Ahmed";
+
+// Reverse the name
+let reversedName = name.split('').reverse().join('');
+
+// Output the result
+console.log(reversedName); // Output: demhA maluG
