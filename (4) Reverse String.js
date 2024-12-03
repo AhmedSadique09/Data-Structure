3be6616f23c622ad3b31337bda5e3f3c11e1bb15
Ghulam Ahmed <dev.ahmed09@gmail.com>
2024-12-03 13:55:57 +0500

    .

--- (4) Reverse String.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
-}
-
-let originalString = "Ahmed";
-let reversedString = reverseString(originalString);
-
-console.log(reversedString);
